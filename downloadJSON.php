595cb054b70d0d923990ee41c41a9ec6c707149f
Ricardo Quintas <rqcenter-web@example.com>
2015-05-19 18:48:49 +0100

    download JSON fix

---
+++ downloadJSON.php
@@ -0,0 +1,12 @@
+<?php
+if (isset ( $_GET ['data'] )) {
+	$json = $_GET ['data'];
+	$filename = $_GET ['filename'];
+	header ( "Content-disposition: attachment; filename='$filename'" );
+	header ( 'Content-type: application/json' );	
+	echo $json;
+} else {
+	echo "Erro na criacao do ficheiro";
+}
+
+?>
