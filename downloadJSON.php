45a94723d65ce0661ba6a50f951c1f461b629004
Ricardo Quintas <rqcenter-web@yahoo.com>
2015-05-29 00:42:52 +0100

    final fixes

--- downloadJSON.php
+++ downloadJSON.php
@@ -3,7 +3,7 @@ if (isset($_POST['data'])) {
 	$json = $_POST['data'];
 	$filename = $_POST['filename'];
 	header("Content-disposition: attachment; filename='$filename'");
-	header('Content-type: application/json');
+	header('Content-type: application/json; charset=utf-8');
 	echo urldecode($json);
 } else {
 	echo "Erro na criacao do ficheiro";
